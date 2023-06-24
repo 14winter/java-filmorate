108cecda95229acdc0779143b97dd6ae15a0e41f
Руслан Захаров <rsln.zakharov@yandex.ru>
2023-06-25 03:48:12 +0900

    ТЗ11_6_Ревью_1

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/impl/FriendDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/impl/FriendDbStorage.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Component
 @Slf4j
 public class FriendDbStorage implements FriendStorage {
-
+    
     private final JdbcTemplate jdbcTemplate;
 
     public FriendDbStorage(JdbcTemplate jdbcTemplate) {
